73fccc83f9b84357048c68ad0313293ca9cc4b7a
Yuri Mikhin <urasergeevich@mail.ru>
2023-02-20 13:51:46 +0300

    Add oklch colors.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  plugins: [],
+  theme: {
+    colors: {
+      white: "oklch(100% 0 0)",
+      black: "oklch(26% 0 0)",
+      pink: "oklch(89% 0.08 0)",
+      yellow: "oklch(89% 0.14 84)",
+      green: "oklch(89% 0.14 123)",
+      turquoise: "oklch(89% 0.14 162)",
+    },
+  },
 };
