9ecbf2540d70436765bca6bb559da40916161234
Yuri Mikhin <urasergeevich@mail.ru>
2023-03-07 15:01:34 +0300

    Add https for local dev.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// need https locally, because of Contact Picker API working only on secure contexts
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [react()],
+  server: { https: true },
+  plugins: [mkcert(), react()],
 });
